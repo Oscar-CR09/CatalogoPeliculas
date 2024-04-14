9128581fa2e596277458d41132600b65ab575adc
Oscar <oscarcr189@outlook.com>
2024-04-14 16:55:59 -0600

    Inicio de proyecto

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Bienvenido(a) a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        
+
+    }
+}

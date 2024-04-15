203c6074053846205d5ad679eab3a91b4e8540c4
Oscar <oscarcr189@outlook.com>
2024-04-14 18:20:36 -0600

    variables

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,19 @@ public class Main {
 
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
-        
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+
+        System.out.println(media);
+
+        String sinopsis = """
+                 Matrix es una paradoja 
+                 La mejor pelicula del fin del milenio
+                 Fue Lanzada en:
+                 """ + fechaDeLanzamiento;
+
+        System.out.println(sinopsis);
 
     }
 }

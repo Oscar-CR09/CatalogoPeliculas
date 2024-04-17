9d5d06d89751ae4940f1e6e21f86a4f5d7abcad1
Oscar <oscarcr189@outlook.com>
2024-04-16 22:48:08 -0600

    Loops con escanner

--- src/Loops.java
+++ src/Loops.java
@@ -6,6 +6,18 @@ public class Loops {
 
         Scanner teclado = new Scanner(System.in);
 
+        double nota = 0;
+        double mediaEvaluaciones = 0;
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Escribe la nota que le darias a la pelicula matrix");
+            nota = teclado.nextDouble();
+
+            mediaEvaluaciones=mediaEvaluaciones + nota;
+        }
+
+        System.out.println("La media de evaluaciones para matrix es : " + mediaEvaluaciones/3);
 
     }
 }
